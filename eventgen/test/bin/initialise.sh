4ec395fc6d02bb1a1937562d27ac8b0ecf854fa4
gcdev373 <gcdev373@users.noreply.github.com>
2020-11-09 11:10:33 +0000

    Fix shellcheck warnings.

--- eventgen/test/bin/initialise.sh
+++ eventgen/test/bin/initialise.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "This should be run only once to send an initial seed set of reference data to Stroom, to avoid errors"
-echo "usage: $0 [host count] [user count] [stream count] [install directory]"
+echo "You can provide optional arguments via command line paramaters: $0 [host count] [user count] [stream count] [install directory]"
 
 if (( $# > 0 ));
 then
@@ -36,22 +36,29 @@ OUTPUT_DIR=$INSTALL_DIR/output
 
 cd "$INSTALL_DIR"
 
-echo "Generating test data with $HOST_COUNT hosts, $USER_COUNT users, and $STREAM_COUNT event substreams per batch"
+if (( $? != 0 ))
+then
+  echo "Fatal, unable to change to eventgen install directory"
+  exit
+fi
 
-echo "Please wait..."
-mkdir -p $OUTPUT_DIR
+echo "Generating test data with $HOST_COUNT hosts, $USER_COUNT users, and $STREAM_COUNT event substreams per batch"
+echo "Please allow this process up to one hour to complete..."
+echo
+mkdir -p "$OUTPUT_DIR"
 
 echo "Creating reference data required for repeatable tests"
 java -Xmx40M -jar bin/eventgen.jar config/reference/hosts.yml -p 2019-12-01T00:00:00.000Z -r P31D -h $HOST_COUNT -u $USER_COUNT
 java -Xmx40M -jar bin/eventgen.jar config/reference/example.yml -p 2019-12-01T00:00:00.000Z -r P31D -h $HOST_COUNT -u $USER_COUNT
 
 echo "Sending that data to Stroom"
-bin/sendAllZipsToStroom $OUTPUT_DIR
+bin/sendAllZipsToStroom "$OUTPUT_DIR"
 
 echo "Generating event data for repeatable tests"
+
 java -Xmx40M -jar bin/eventgen.jar config/events/example.yml -p 2019-12-01T00:00:00.000Z -r P31D -h $HOST_COUNT -u $USER_COUNT
 sleep 300
-bin/sendAllZipsToStroom $OUTPUT_DIR
+bin/sendAllZipsToStroom "$OUTPUT_DIR"
 
 echo "Providing some more recent reference data for use with event data created by invoke.sh"
 
@@ -84,11 +91,14 @@ bin/eventgen config/reference/example.yml refexample state DAY 2020-01-01 ROLLIN
 bin/eventgen config/reference/hosts.yml hosts state DAY 2020-01-01 ROLLING -h $HOST_COUNT -u $USER_COUNT 
 bin/eventgen config/reference/example.yml refexample state DAY 2020-01-01 ROLLING -h $HOST_COUNT -u $USER_COUNT
 
-bin/sendAllZipsToStroom $OUTPUT_DIR
+bin/sendAllZipsToStroom "$OUTPUT_DIR"
 echo "Sleeping to allow Stroom time to process this initial data..."
 sleep 150
 echo "Flushing index shards"
 util/flushAllShards
 echo "Done. "
-echo "You can now run runTestSearch.sh to check that processing has been successful."
-echo "You may also run invoke.sh, or install it in crontab"
+echo "To comfirm that processing has been successful, the following scripts may be run:"
+echo "util/runTestSearch.sh checks that the results of a search are as expected."
+echo "test/bin/findErrorStreams.sh checks for error streams associated with eventgen processing"
+echo
+echo "You may now run invoke.sh, or install it to run regularly (e.g. every minute) in crontab"
